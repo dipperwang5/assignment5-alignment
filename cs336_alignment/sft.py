44b94039b2ea93d0c289fdffc1b0796a820312a6
dipperwang5 <dipper.wang5@gmail.com>
2025-09-06 02:00:05 +0000

    sft script

--- cs336_alignment/sft.py
+++ cs336_alignment/sft.py
@@ -146,3 +146,235 @@ def sft_microbatch_train_step(
     adjusted_normalized_loss.backward()
 
     return adjusted_normalized_loss, {"normalized_loss": normalized_loss}
+
+
+def log_generations(model, vllm, eval_prompts, eval_answers, reward_fn,
+                    sampling_params, total_train_steps: int, num_samples: int = 100,
+                    full_dataset: bool = False):
+    load_policy_into_vllm_instance(model, vllm)
+
+    # randomly sample a set of indices
+    if full_dataset:
+        eval_prompts_sample = eval_prompts
+        eval_answers_sample = eval_answers
+    else:
+        indices = random.sample(range(len(eval_prompts)), num_samples)
+        eval_prompts_sample = [eval_prompts[i] for i in indices]
+        eval_answers_sample = [eval_answers[i] for i in indices]
+
+    eval_results = evaluate_vllm(vllm, reward_fn, eval_prompts_sample, eval_answers_sample, sampling_params)
+
+    correct_lengths = []
+    incorrect_lengths = []
+    format_reward = 0
+    answer_reward = 0
+    for info_dict in eval_results:
+        if info_dict["answer_reward"] == 1:
+            correct_lengths.append(len(info_dict["response"]))
+        else:
+            incorrect_lengths.append(len(info_dict["response"]))
+        format_reward += info_dict["format_reward"]
+        answer_reward += info_dict["answer_reward"]
+
+    correct_length = sum(correct_lengths) / len(correct_lengths) if correct_lengths else 0
+    incorrect_length = sum(incorrect_lengths) / len(incorrect_lengths) if incorrect_lengths else 0
+    average_length = sum(correct_lengths + incorrect_lengths) / len(correct_lengths + incorrect_lengths) if correct_lengths + incorrect_lengths else 0
+
+    avg_format_reward = format_reward / len(eval_results)
+    avg_answer_reward = answer_reward / len(eval_results)
+    wandb.log({"eval/correct_length": correct_length,
+               "eval/incorrect_length": incorrect_length,
+               "eval/average_length": average_length,
+               "eval/format_reward": avg_format_reward,
+               "eval/answer_reward": avg_answer_reward,
+               "eval_step": total_train_steps})
+
+    # print one sample
+    # print(f"Prompt: {eval_results[0]['prompt']}")
+    # print(f"Answer: {eval_results[0]['answer']}")
+    # print(f"Response: {eval_results[0]['response']}")
+    # print(f"Format Reward: {eval_results[0]['format_reward']}")
+    # print(f"Answer Reward: {eval_results[0]['answer_reward']}")
+
+    return avg_answer_reward, avg_format_reward
+
+
+def load_stf_data(data_path: str, data_amount: int) -> tuple[List[str], List[str], List[str]]:
+    prompts = []
+    answers = []
+    cots = []
+    with open(data_path, "r") as data_file:
+        for line in data_file:
+            data = json.loads(line)
+            prompts.append(data["prompt"])
+            cots.append(data["response"])
+            answers.append(data["ground_truth"])
+    
+    if data_amount == -1:
+        return prompts, cots, answers
+    else:
+        return prompts[: data_amount], cots[: data_amount], answers[: data_amount]
+
+def sft_training_loop(
+    model: PreTrainedModel,
+    tokenizer: PreTrainedTokenizer,
+    llm: LLM,
+    sft_prompts: List[str],
+    sft_cots: List[str],
+    sft_answers: List[str],
+    gradient_accumulation_steps: int,
+    microbatch_size: int,
+    device: str,
+    eval_prompts: List[str],
+    eval_answers: List[str],
+    eval_steps: int,
+    eval_sampling_params: SamplingParams,
+    output_dir: str,
+    learning_rate: float = 1e-5,
+    max_grad_norm: float | None = 1.0,
+    epochs: int = 10,
+    half_dataset: bool = False,
+    starting_step: int = 0,
+) -> None:
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+    best_eval_reward = 0
+    total_train_steps = starting_step
+    running_loss = 0
+
+    for epoch in epochs:
+        indices = list(range(len(sft_prompts)))
+        random.shuffle(indices)
+        sft_prompts = [sft_prompts[i] for i in indices]
+        sft_cots = [sft_cots[i] for i in indices]
+        sft_answers = [sft_answers[i] for i in indices]
+        print(f"Epoch {epoch}")
+
+        progress_bar = tqdm(range(0, len(sft_prompts), microbatch_size))
+        for microbatch_idx, i in enumerate(progress_bar):
+            microbatch_prompts = sft_prompts[i:i+microbatch_size]
+            microbatch_cots = sft_cots[i:i+microbatch_size]
+            microbatch_answers = sft_answers[i:i+microbatch_size]
+
+            #log the epoch
+            progress_bar.set_description(f"Epoch {epoch}")
+
+            #tokenize the data
+            tokenize_result = tokenize_prompt_and_output(microbatch_prompts, microbatch_cots, tokenizer)
+            input_ids = tokenize_result["input_ids"].to(device)
+            labels = tokenize_result["labels"].to(device)
+            response_mask = tokenize_result["response_mask"].to(device)
+
+            # model response 
+            model_output_dict = get_response_log_probs(model, input_ids, labels, return_token_entropy=True)
+            policy_log_probs = model_output_dict["log_probs"].to(device)
+            token_entropy = model_output_dict["token_entropy"].to(device)
+
+            train_loss, _ = sft_microbatch_train_step(policy_log_probs, response_mask, gradient_accumulation_steps)
+            running_loss += train_loss.item()
+            if microbatch_idx % gradient_accumulation_steps == 0 or microbatch_idx == len(progress_bar) - 1:
+                if max_grad_norm:
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+                optimizer.step()
+                optimizer.zero_grad()
+
+                total_train_steps += 1
+                avg_loss = running_loss / gradient_accumulation_steps
+                wandb.log({"train/loss": avg_loss,
+                           "train/token_entropy": token_entropy.mean(),
+                           "train_step": total_train_steps})
+
+                progress_bar.set_postfix({"loss": avg_loss, "total_train_steps": total_train_steps})
+                running_loss = 0
+
+            # eval
+            if microbatch_idx % eval_steps == 0:
+                avg_answer_reward, avg_format_reward = log_generations(model, llm, eval_prompts, eval_answers, r1_zero_reward_fn, eval_sampling_params, total_train_steps)
+                print(f"Avg eval reward (total/format): {avg_answer_reward}, {avg_format_reward}")
+                if avg_answer_reward > best_eval_reward:
+                    print("Saving best model")
+                    best_eval_reward = avg_answer_reward
+                    save_model_tokenizer(model, tokenizer, output_dir, "best")
+
+    # eval vllm
+    if half_dataset:
+        avg_answer_reward, avg_format_reward = log_generations(model, llm, eval_prompts, eval_answers, r1_zero_reward_fn, eval_sampling_params, total_train_steps, full_dataset=False, num_samples=1000)
+    else:
+        avg_answer_reward, avg_format_reward = log_generations(model, llm, eval_prompts, eval_answers, r1_zero_reward_fn, eval_sampling_params, total_train_steps, full_dataset=True)
+
+    print(f"Avg eval reward (total/format): {avg_answer_reward}, {avg_format_reward}")
+    if avg_answer_reward > best_eval_reward:
+        print("Saving best model")
+        save_model_tokenizer(model, tokenizer, output_dir, "best")
+
+    return total_train_steps
+
+
+def main(sft_data_path: str, eval_data_path: str, model_path: str, output_dir: str,
+         microbatch_size: int, gradient_accumulation_steps: int, data_amount: int,
+         eval_steps: int, epochs: int, learning_rate: float = 1e-5, max_grad_norm: float = 1.0, experiment_name: str = "sft",
+         from_filtered: bool = False):
+    setup_wandb(experiment_name)
+
+    # load model and tokenizer
+    train_device = "cuda:0"
+    eval_device = "cuda:0"
+    model, tokenizer = load_model_and_tokenizer(model_path)
+    model.to(train_device)
+
+    # optimize model with training data
+    sft_prompts, sft_cots, sft_answers = load_sft_data(sft_data_path, data_amount)
+
+    eval_sampling_params = SamplingParams(
+        temperature=1.0, top_p=1.0, max_tokens=1024, stop=["</answer>"], include_stop_str_in_output=True
+    )
+
+    # eval vllm
+    prompt_path = "prompts/r1_zero.prompt"
+    eval_prompts, eval_answers = load_and_format_prompts(eval_data_path, prompt_path)
+
+    print(f"sft_prompts: {len(sft_prompts)}, sft_answers: {len(sft_answers)}")
+    print(f"eval_prompts: {len(eval_prompts)}, eval_answers: {len(eval_answers)}")
+
+    llm = init_vllm(model_path, device=eval_device, seed=42)
+    load_policy_into_vllm_instance(model, llm)
+    sft_training_loop(model, tokenizer, llm, sft_prompts, sft_cots, sft_answers, gradient_accumulation_steps,
+                      microbatch_size, train_device, eval_prompts, eval_answers,
+                      eval_steps, eval_sampling_params, output_dir, learning_rate=learning_rate, max_grad_norm=max_grad_norm, epochs=epochs)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--microbatch_size", type=int, default=2)
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=4)
+    parser.add_argument("--data_amount", type=int, default=128)
+    parser.add_argument("--output_dir", type=str, default="results/sft")
+    parser.add_argument("--eval_steps", type=int, default=64)
+    parser.add_argument("--epochs", type=int, default=5)
+    parser.add_argument("--learning_rate", type=float, default=5e-5)
+    parser.add_argument("--max_grad_norm", type=float, default=1.0, help="Maximum gradient norm for gradient clipping")
+    parser.add_argument("--experiment_name", type=str, default="sft_128")
+    parser.add_argument("--from_filtered", action="store_true")
+    args = parser.parse_args()
+    print(args.epochs)
+
+    if args.from_filtered:
+        sft_data_path = "data/MATH/sft_filtered.jsonl"
+    else:
+        sft_data_path = "data/MATH/sft.jsonl"
+
+    main(
+        sft_data_path=sft_data_path,
+        eval_data_path="data/MATH/validation.jsonl",
+        model_path=QWEN_BASE_PATH,
+        output_dir=args.output_dir,
+        microbatch_size=args.microbatch_size,
+        gradient_accumulation_steps=args.gradient_accumulation_steps,
+        data_amount=args.data_amount,
+        eval_steps=args.eval_steps,
+        epochs=args.epochs,
+        learning_rate=args.learning_rate,
+        max_grad_norm=args.max_grad_norm,
+        experiment_name=args.experiment_name,
+        from_filtered=args.from_filtered
+    )
